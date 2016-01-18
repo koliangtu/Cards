559471fb98364e717bc0e87dd782f1507f168941
Nikolay Nikiforov <kolia>
2016-01-18 16:49:12 +0600

    edit
    edit numeric

--- scripts/my.js
+++ scripts/my.js
@@ -28,6 +28,9 @@ firstPos();
 
 function firstPos(){
 if($( "li" ).length>1)$( "li" ).last().css( "left", "60px" );
+$( "span" ).each(function( index ) {
+  $( this ).html(index);
+});
 }
 
 $(window).on('popstate',function(event) {
